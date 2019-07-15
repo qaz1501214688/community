92b2ebb44fad8856316c6a882009b8db2214d00a
qaz1501214688 <1501214688@qq.com>
2019-07-15 21:17:35 +0800

    init repo

---
+++ src/main/java/top/xiaoyang666/community/CommunitiyApplication.java
@@ -0,0 +1,13 @@
+package top.xiaoyang666.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitiyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitiyApplication.class, args);
+    }
+
+}
